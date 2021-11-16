4a2680db3eb9dd0e5113bad7c41be09d5f4adfd8
Morten Turn Pedersen <morten.turn-pedersen@monstar-lab.com>
2021-11-16 14:06:45 +0100

    Moved OnModelCreating into their own Configuration files

---
+++ Monstarlab.Templates.API.Infrastructure.Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace Monstarlab.Templates.API.Infrastructure.Data.Configurations;
+
+internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    internal static Guid AarhusId = new Guid("4f1759f7-58fa-429b-a7b9-2fb375258df3");
+    internal static Guid CopenhagenId = new Guid("d8f7f42e-23a6-48b8-af97-a8cc3cb6c58b");
+
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        var aarhus = new Department
+        {
+            Id = AarhusId,
+            City = "Aarhus",
+            Country = "Denmark",
+            Floor = "9",
+            Number = "2F",
+            Street = "Mariane Thomsens Gade",
+            ZipCode = "8000"
+        };
+        var copenhagen = new Department
+        {
+            Id = CopenhagenId,
+            City = "Copenhagen",
+            Country = "Denmark",
+            Street = "Orientkaj",
+            Number = "4",
+            ZipCode = "2150"
+        };
+
+        builder.HasData(new[]
+            {
+                aarhus,
+                copenhagen
+            });
+    }
+}
---
+++ Monstarlab.Templates.API.Infrastructure.Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace Monstarlab.Templates.API.Infrastructure.Data.Configurations;
+
+internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder
+            .HasOne<Department>(e => e.Department)
+            .WithMany(d => d.Employees)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasData(new[]
+            {
+                new Employee
+                {
+                    Id = new Guid("efb8f31e-cdd3-4d6b-96a3-3ee6fe9ae679"),
+                    FirstName = "Morten",
+                    LastName = "Turn Pedersen",
+                    Age = 32,
+                    DepartmentId = DepartmentConfiguration.AarhusId
+                },
+                new Employee
+                {
+                    Id = new Guid("35607bf7-9a00-489d-bf71-bbdb53f2f7d8"),
+                    FirstName = "Morten",
+                    LastName = "Pløger",
+                    Age = 29,
+                    DepartmentId = DepartmentConfiguration.AarhusId
+                },
+                new Employee
+                {
+                    Id = new Guid("fa4b4f52-0c8b-4839-bbc1-d33a9bf2ca38"),
+                    FirstName = "Kasper",
+                    LastName = "Welner",
+                    Age = 31,
+                    DepartmentId = DepartmentConfiguration.CopenhagenId
+                }
+            });
+    }
+}
